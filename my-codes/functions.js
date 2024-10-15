029b67254ce5397998097bbe0b0092786afc7f07
Taiyeadenike <151226143+Taiyeadenike@users.noreply.github.com>
2024-10-15 12:32:33 +0100

    Update functions.js

--- my-codes/functions.js
+++ my-codes/functions.js
@@ -1,49 +1,3 @@
-function workSmart() {
-    console.log("work smart not hard");
-    console.log("work smart not hard")
-    console.log("work smart not hard")
-    console.log("work smart not hard")
-}
-
-function add(number) {
-    
-    if (2 % 10) {
-        return true
-    }
-    else {
-        return false
-    }
-}
-
-// function add(number) {
-//     if (2 % 10) {
-//         console.log("true")
-//     }
-//     else {
-//         console.log("false")
-//     }
-// }
-
-// function evenN(number) {
-    
-//     if (number % 2 === 0) {
-//         return true
-//     }
-//     else {
-//         return fasle
-//     }
-// }
-// console.log(evenN(9))
-
-// function even() {
-//     if (2 % 0) {
-//         console.log("true");
-//     }
-//     else {
-//         console.log("false");
-//     }
-// }
-
 
 function sum(a, b, c) {
     return a + b + c
@@ -51,17 +5,6 @@ function sum(a, b, c) {
 console.log(sum(2, 4, 3));
 
 
-
-let number = 2
-function evenN(number) {
-    if (number % 2 == 0) {
-        return true
-    }
-    else {
-        return false
-    }
-}
-console.log(evenN)
 
 
 function evenN(number) {
